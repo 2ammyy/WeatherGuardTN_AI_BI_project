889e2a3b0fa9b4f94a050a6d1fcdf8785fe89095
Zahrat-ElOula <zahrayessine@gmail.com>
2026-03-05 00:09:34 +0100

    frontend enhancement with react + dashboard

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Import the new plugin
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Add it here
-  ],
+  plugins: [react()],
+  server: {
+    port: 5173,
+    open: true
+  }
 })
